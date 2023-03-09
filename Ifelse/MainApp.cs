7c73db940f91db31db959eec01b17a8d719b26cf
bohyeong99 <bohyeong99@gmail.com>
2023-03-09 15:28:25 +0900

    20230309/1527

--- Ifelse/MainApp.cs
+++ Ifelse/MainApp.cs
@@ -1,15 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ifelse
-{
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-}
+﻿using System
