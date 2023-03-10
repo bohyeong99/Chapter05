e751cb9f5fc9361d1bf0c4c5b47324fb9103778a
bohyeong99 <bohyeong99@gmail.com>
2023-03-10 15:30:49 +0900

    ifelse 예제

--- Ifelse/MainApp.cs
+++ Ifelse/MainApp.cs
@@ -1,7 +1,42 @@
 ﻿using System;
 
-namespace IFelse
+namespace Ifelse
 {
     class MainApp
-    { //DWDAADWD}
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+            {
+                Console.WriteLine("음수");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("양수");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }
+            else
+            {
+                Console.WriteLine("홀수");
+            }
+            {
+                string result(number % 2 == 0) == 0 ? "짝수" : "홀수";
+                Console.WriteLine(result);
+            }
+
+        }
+    }
 }
